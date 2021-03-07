4d5e10629b2e6ecf9a171d433ac895d52cfaf8ce
Lauri Koskela <luryus@users.noreply.github.com>
2021-03-07 11:35:23 +0200

    WIP

---
+++ Kyyla/AbsenceWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Kyyla.ViewModel;
+using ReactiveUI;
+
+namespace Kyyla
+{
+    /// <summary>
+    /// Interaction logic for AbsenceWindow.xaml
+    /// </summary>
+    public partial class AbsenceWindow : ReactiveWindow<AbsenceViewModel>
+    {
+        public AbsenceWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kyyla/ViewModel/AbsenceViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ReactiveUI;
+using System;
+using DynamicData;
+
+namespace Kyyla.ViewModel
+{
+    public class AbsenceViewModel : ReactiveObject
+    {
+        public class RowViewModel : ReactiveObject
+        {
+            private DateTimeOffset _startTime;
+            public DateTimeOffset StartTime
+            { 
+                get => _startTime; 
+                set => this.RaiseAndSetIfChanged(ref _startTime, value);
+            }
+
+            private DateTimeOffset _endTime;
+            public DateTimeOffset EndTime
+            {
+                get => _startTime;
+                set => this.RaiseAndSetIfChanged(ref _endTime, value);
+            }
+
+            public DynamicData.
+        }
+    }
+}
